853258081dc8ae8a773e07c2b246801826447708
Ivan Korobov <ivankor48@gmail.com>
2023-12-22 13:44:34 +0300

    Решение задачи 2.1.3. Spring Beans

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,12 +1,23 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
+    private Timer timer;
     @Autowired
+    public AnimalsCage(@Qualifier("Timer") Timer timer) {
+        this.timer = timer;
+    }
+    public Timer getTimer() {
+        return timer;
+    }
+
+    @Autowired
+    @Qualifier("Dog")
     private Animal animal;
 
     public void whatAnimalSay() {
